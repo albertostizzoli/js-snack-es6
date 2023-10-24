f6a12c781b3f9eeff8491aa58405a13775a2d184
albertostizzoli <albertostizzoli@yahoo.it>
2023-10-24 14:43:44 +0200

    Create List of Students in jsnack2

---
+++ jsnack2/js/script.js
@@ -0,0 +1,42 @@
+// Recupero gli elementi in pagina
+const display = document.getElementById('display');
+
+const students = [
+    {
+        name:'Marco della Rovere',
+        id: 0,
+        grades: 0
+    },
+    {
+        name:'Paola Cortellessa',
+        id: 0,
+        grades: 0
+    },
+    {
+        name:'Andrea Mantegna',
+        id: 0,
+        grades: 0
+    },
+    {
+        name:'Gaia Borromini',
+        id: 0,
+        grades: 0
+    },
+    {
+        name:'Luigi Grimaldello',
+        id: 0,
+        grades: 0
+    },
+    {
+        name:'Piero della Francesca',
+        id: 0,
+        grades: 0
+    },
+    {
+        name:'Francesca da Polenta',
+        id: 0,
+        grades: 0
+    },
+];
+
+console.log(students);
