b7d071208c19be31ae5a2d665578cb7ab2329917
albertostizzoli <albertostizzoli@yahoo.it>
2023-10-25 16:04:17 +0200

    Complete jsnack3

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ESERCIZIO DI OGGI: SNACK ARRAY METHODS
 
-1. SNACK 1:
+1.  SNACK 1:
 Dwayne Johnson ci ha chiesto di creare i segnaposto per il tavolo degli invitati alla sua mega festa vip.
 Ci ha lasciato il nome del tavolo ("Tavolo Vip") e la lista degli invitati in ordine di posto:
 1. Brad Pitt
---
+++ jsnack3/js/script.js
@@ -0,0 +1,39 @@
+// Creo array di oggetti contenente le caratteristiche delle varie bici
+const listaBici = [
+    {
+        nome: 'Bianchi',
+        peso: 15
+    },
+    {
+        nome: 'Cannondale',
+        peso: 18
+    },
+    {
+        nome: 'Santa Cruz',
+        peso: 10
+    },
+    {
+        nome: 'Megamo',
+        peso: 7
+    },
+    {
+        nome: 'DinoBikes',
+        peso: 9
+    },
+];
+
+console.log(listaBici);
+
+// Con un forEach e un filter trovo la bici con il peso minore all'interno della lista
+const pesoMinore = [];
+listaBici.forEach((element, index)=> {
+    const {peso} = listaBici[index];
+    pesoMinore.push(peso);
+})
+const res = listaBici.filter((element)=>{
+    return element.nome && element.peso === Math.min(...min);
+});
+
+// Infine stampo in console la bici che pesa di meno
+console.log(`La bici che pesa di meno è: ${res[0].nome} e pesa ${res[0].peso}kg`);
+
